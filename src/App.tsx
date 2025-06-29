60254e07e7db9b43f2e10446f2337463cbec1ba9
mstne03 <mserraitorne@gmail.com>
2025-06-29 14:27:07 +0200

    Creates hero-section branch

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import HeaderSection from './components/HeaderSection/HeaderSection'
 import bookmarkLogo from './assets/Imagenes/logo-bookmark.svg'
 import menuImg from './assets/Imagenes/icon-hamburger.svg'
+import HeroSection from './components/HeroSection/HeroSection'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <HeaderSection 
-      logoImg={bookmarkLogo}
-      menuImg={menuImg}
-    />
+    <>
+      <HeaderSection 
+        logoImg={bookmarkLogo}
+        menuImg={menuImg}
+      />
+
+      <HeroSection/>
+    </>
   )
 }
 
