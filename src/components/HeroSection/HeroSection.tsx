57f583fca489f0d1d60ff14e3f0e2478fa86a85d
mstne03 <mserraitorne@gmail.com>
2025-06-29 18:44:03 +0200

    Creates second sub-section for hero-section.

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,27 +1,42 @@
 interface HeroSectionProps {
-  imgSrc : string
+  imgSrc: string
 }
 
-const HeroSection = (props : HeroSectionProps) => (
-  <main className="px-10">
-    <div className="
-            relative
-            after:content-['']
-            after:absolute
-            after:bg-blue-500
-            after:rounded-full
-            after:-z-10
-            after:w-100
-            after:h-45
-            after:top-10
-            after:left-10
-          "
-    >
-      <img 
-        src={props.imgSrc}
-        alt="hero-image"
-        className="object-contain z-10 absolute right-3"
-      />
+const HeroSection = (props: HeroSectionProps) => (
+  <main className="px-10 flex flex-col justify-between space-y-65">
+    <div className="relative">
+      <div className="
+              after:content-['']
+              after:absolute
+              after:bg-blue-500
+              after:rounded-full
+              after:-z-10
+              after:w-100
+              after:h-45
+              after:top-10
+              after:left-10
+            "
+      >
+        <img
+          src={props.imgSrc}
+          alt="hero-image"
+          className="object-contain z-10 absolute right-3"
+        />
+      </div>
+    </div>
+
+    <div className="space-y-7">
+      <h1 className="text-3xl text-center font-medium">
+        A Simple Bookmark Manager
+      </h1>
+
+      <p className="text-center">
+        A clean and simple interface to organize your 
+        favourite websites. Open a new browser tab and 
+        see your sites load instantly. Try it for free.
+      </p>
+
+      
     </div>
   </main>
 );
