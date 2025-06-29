48c0d200c50467a9fbebc10eeb6fd665526dca2d
mstne03 <mserraitorne@gmail.com>
2025-06-29 13:46:09 +0200

    Creates develop branch

---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface HeroSectionProps {}
+
+const HeroSection: FC<HeroSectionProps> = () => (
+  <div>
+    HeroSection Component
+  </div>
+);
+
+export default HeroSection;
