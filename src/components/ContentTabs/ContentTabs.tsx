1b042af2441f9dcfcc148cbf6dbe2e6b361a3e0a
mstne03 <mserraitorne@gmail.com>
2025-06-30 00:38:34 +0200

    Changing return for content-tabs component in order to use hooks

--- src/components/ContentTabs/ContentTabs.tsx
+++ src/components/ContentTabs/ContentTabs.tsx
@@ -4,38 +4,40 @@ interface ContentTabsProps {
   
 }
 
-const ContentTabs = (props:ContentTabsProps) => (
-  <div className="flex flex-col justify-center">
-    <div className="flex flex-col justify-center border-y-1 border-y-gray-400">
-      <ButtonComponent 
-        text="Simple Bookmarking"
-        bg_color="bg-transparent"
-        text_color=""
-        borders = "border-2 border-transparent border-b-2 border-b-red"
-        shadow = ""
-      />
-    </div>
+const ContentTabs = (props:ContentTabsProps) => {
+  return (
+    <div className="flex flex-col justify-center mx-2">
+      <div className="flex flex-col justify-center border-y-1 border-y-gray-400">
+        <ButtonComponent
+          text="Simple Bookmarking"
+          bg_color="bg-transparent"
+          text_color=""
+          borders = "border-2 border-transparent border-b-2 border-b-red"
+          shadow = ""
+        />
+      </div>
 
-    <div className="flex flex-col justify-center border-y-1 border-y-gray-400">
-      <ButtonComponent 
-        text="Speedy Searching"
-        bg_color="bg-transparent"
-        text_color=""
-        borders = "border-2 border-transparent border-b-2 border-b-red"
-        shadow = ""
-      />
-    </div>
+      <div className="flex flex-col justify-center border-y-1 border-y-gray-400">
+        <ButtonComponent 
+          text="Speedy Searching"
+          bg_color="bg-transparent"
+          text_color=""
+          borders = "border-2 border-transparent border-b-2 border-b-red"
+          shadow = ""
+        />
+      </div>
 
-    <div className="flex flex-col justify-center border-y-1 border-y-gray-400">
-      <ButtonComponent 
-        text="Easy Sharing"
-        bg_color="bg-transparent"
-        text_color=""
-        borders = "border-2 border-transparent border-b-2 border-b-red"
-        shadow = ""
-      />
+      <div className="flex flex-col justify-center border-y-1 border-y-gray-400">
+        <ButtonComponent 
+          text="Easy Sharing"
+          bg_color="bg-transparent"
+          text_color=""
+          borders = "border-2 border-transparent border-b-2 border-b-red"
+          shadow = ""
+        />
+      </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default ContentTabs;
