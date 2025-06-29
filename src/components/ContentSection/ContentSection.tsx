da37afcf7a244f5bdec8d7ccb00742d9b24df916
mstne03 <mserraitorne@gmail.com>
2025-06-29 18:03:28 +0200

    Creates ContentSection component

---
+++ src/components/ContentSection/ContentSection.tsx
@@ -0,0 +1,11 @@
+interface ContentSectionProps {
+  
+}
+
+const ContentSection = (props:ContentSectionProps) => (
+  <div>
+    ContentSection Component
+  </div>
+);
+
+export default ContentSection;
