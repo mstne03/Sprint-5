226d07b8918b53d3ccd0953b0b2d438b2a52be87
mstne03 <mserraitorne@gmail.com>
2025-07-01 20:56:56 +0200

    feature - Adds pc displayable elements to header component

--- src/components/HeaderSection/HeaderSection.tsx
+++ src/components/HeaderSection/HeaderSection.tsx
@@ -1,3 +1,4 @@
+import ButtonComponent from '../ButtonComponent/ButtonComponent'
 
 interface HeaderSectionProps {
   logoImg : string,
@@ -5,21 +6,43 @@ interface HeaderSectionProps {
 }
 
 const HeaderSection = (props: HeaderSectionProps) => (
-  <header className="flex justify-between items-center py-12 px-10">
+  <header className="flex justify-between items-center py-12 px-10 md:px-20">
     <span>
       <img 
-        className="object-contain max-w-35"
+        className="object-contain max-w-35 sm:min-w-40"
         src={props.logoImg}
         alt="bookmark_logo"
       />
     </span>
 
-    <span>
+    <span className="sm:hidden">
       <img 
         src={props.menuImg} 
         alt="hamburger_icon" 
       />
     </span>
+
+    <nav className="hidden sm:flex sm:justify-center sm:items-center sm:gap-5">
+      <ul className="flex gap-6">
+        <li>
+          <a href="#">FEATURES</a>
+        </li>
+        <li>
+          <a href="#">PRICING</a>
+        </li>
+        <li>
+          <a href="#">CONTACT</a>
+        </li>
+      </ul>
+
+      <ButtonComponent
+        text="LOGIN"
+        textColor="text-white"
+        bg_color="bg-red-500"
+        paddingX="px-7"
+        paddingY="py-3"
+      />
+    </nav>
   </header>
 );
 
