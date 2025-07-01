35c768f4c6a6e8298eeda1ef1e8e6524c831b25f
mstne03 <mserraitorne@gmail.com>
2025-07-01 12:07:05 +0200

    feature - Creates DownloadSection component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import HeaderSection from './components/HeaderSection/HeaderSection'
 import bookmarkLogo from './assets/Imagenes/logo-bookmark.svg'
 import menuImg from './assets/Imagenes/icon-hamburger.svg'
 import HeroSection from './components/HeroSection/HeroSection'
-import heroImg from './assets/Imagenes/illustration-hero.svg'
 import ContentSection from './components/ContentSection/ContentSection'
+import DownloadSection from './components/DownloadSection/DownloadSection'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,11 +16,11 @@ function App() {
         menuImg={menuImg}
       />
 
-      <HeroSection
-      imgSrc={heroImg}
-      />
+      <HeroSection/>
 
       <ContentSection/>
+
+      <DownloadSection/>
     </>
   )
 }
